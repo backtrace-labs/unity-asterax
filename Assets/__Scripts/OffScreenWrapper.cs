78347db9f79d1b0e6948cc9c2cabd9891a3988ab
Vincent Lussenburg <vincent@lussenburg.net>
2021-01-27 10:56:41 -0700

    fix imports

--- Assets/__Scripts/OffScreenWrapper.cs
+++ Assets/__Scripts/OffScreenWrapper.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// When a GameObject exits the bounds of the OnScreenBounds, screen wrap it.
--- Assets/__Scripts/PlayerShip.cs
+++ Assets/__Scripts/PlayerShip.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerShip : MonoBehaviour
